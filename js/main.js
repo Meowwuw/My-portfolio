dbbcc6a03ad08c4b8ef255ed9609255e4c02ab57
Magenta Paredes <paredesponcemagenta@gmail.com>
2023-12-14 00:48:53 -0500

    Enviando el proyecto

--- js/main.js
+++ js/main.js
@@ -1,134 +1,72 @@
-const titles = ['>App developer 👩‍💻','>UI Designer', '>Web Developer'];
-let currentTitle = 0;
-let currentChar = 0;
-let deleting = false;
+$.fn.boom = function(e) {
+	var colors = [
+		'#ffb3f6',
+		'#7aa0ff',
+		'#333',
+		// '#FFD100',
+		// '#FF9300',
+		// '#FF7FA4'
+	];
+	var shapes = [
+		'<polygon class="star" points="21,0,28.053423027509677,11.29179606750063,40.97218684219823,14.510643118126104,32.412678195541844,24.70820393249937,33.34349029814194,37.989356881873896,21,33,8.656509701858067,37.989356881873896,9.587321804458158,24.70820393249937,1.0278131578017735,14.510643118126108,13.94657697249032,11.291796067500632"></polygon>', 
+		// '<path class="circle" d="m 20 1 a 1 1 0 0 0 0 25 a 1 1 0 0 0 0 -25"></path>',
+		'<polygon class="other-star" points="18,0,22.242640687119284,13.757359312880714,36,18,22.242640687119284,22.242640687119284,18.000000000000004,36,13.757359312880716,22.242640687119284,0,18.000000000000004,13.757359312880714,13.757359312880716"></polygon>',
+		'<polygon class="diamond" points="18,0,27.192388155425117,8.80761184457488,36,18,27.19238815542512,27.192388155425117,18.000000000000004,36,8.807611844574883,27.19238815542512,0,18.000000000000004,8.80761184457488,8.807611844574884"></polygon>'
+	];
 
-const subtitle = document.getElementById('rotating-text');
+	var btn = $(this);
+	var group = [];
+	var num = Math.floor(Math.random() * 50) + 30;
 
-function updateText() {
-  if (deleting) {
-    // Reduce el número de caracteres si está en modo "borrar"
-    if (currentChar > 0) {
-      currentChar--;
-      subtitle.textContent = titles[currentTitle].substring(0, currentChar);
-    } else {
-      // Cambia al siguiente título y cambia el modo a "escribir"
-      deleting = false;
-      currentTitle = (currentTitle + 1) % titles.length;
-    }
-  } else {
-    // Aumenta el número de caracteres si está en modo "escribir"
-    if (currentChar < titles[currentTitle].length) {
-      currentChar++;
-      subtitle.textContent = titles[currentTitle].substring(0, currentChar);
-    } else {
-      // Comienza a borrar después de un retraso cuando se ha terminado de escribir
-      setTimeout(() => { deleting = true; }, 2000);
-    }
-  }
-}
+	for(i = 0; i < num; i++) {
+		var randBG = Math.floor(Math.random() * colors.length);
+		var getShape = Math.floor(Math.random() * shapes.length);
+		var c = Math.floor(Math.random() * 10) + 5;
+		var scale = Math.floor(Math.random() * (8 - 4 + 1)) + 4;
+		var x = Math.floor(Math.random() * (150 + 100)) - 100;
+		var y = Math.floor(Math.random() * (150 + 100)) - 100;
+		var sec = Math.floor(Math.random() * 1700) + 1000;
+		var cir = $('<div class="cir"></div>');
+		var shape = $('<svg class="shape">'+shapes[getShape]+'</svg>');
+		
+		shape.css({
+			top: e.pageY - btn.offset().top + 20,
+			left: e.pageX - btn.offset().left + 40,
+			'transform': 'scale(0.'+scale+')',
+			'transition': sec + 'ms',
+			'fill': colors[randBG]
+		});
 
-// Iniciar la actualización del texto
-setInterval(updateText, 200); // Ajusta la velocidad de tipeo y borrado aquí
+		btn.siblings('.btn-particles').append(shape);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const sidebar = document.querySelector('.sidebar');
-  const sidebarItems = document.querySelectorAll('.sidebar-item');
+		group.push({shape: shape, x: x, y: y});
+	}
+	
+	for (var a = 0; a < group.length; a++) {
+		var shape = group[a].shape;
+		var x = group[a].x, y = group[a].y;
+		shape.css({
+			left: x + 50,
+			top: y + 15,
+			'transform': 'scale(0)'
+		});
+	}
+	
+	setTimeout(function() {
+		for (var b = 0; b < group.length; b++) {
+			var shape = group[b].shape;
+			shape.remove();
+		}
+		group = [];
+	}, 2000);
 
-  function changeSidebarColor(colorClass) {
-    // Eliminar solo las clases de color de fondo
-    sidebar.classList.remove('sidebar-highlight-about', 'sidebar-highlight-projects', 'sidebar-highlight-contact');
-    // Añadir la nueva clase de color de fondo
-    sidebar.classList.add(colorClass);
-  }
-  
+}	
 
-  sidebarItems.forEach(item => {
-    item.addEventListener('click', () => {
-      // Decide qué clase de color agregar en función del ID del ítem clickeado
-      switch (item.id) {
-        case 'link-about-me':
-          changeSidebarColor('sidebar-highlight-about');
-          break;
-        case 'link-projects':
-          changeSidebarColor('sidebar-highlight-projects');
-          break;
-        case 'link-contact':
-          changeSidebarColor('sidebar-highlight-contact');
-          break;
-        
-      }
-    });
-  });
+$(function() {
+	$(document).on('click', '.btn', function(e) {
+		$(this).boom(e);
+	});
+
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Elementos de la barra lateral
-  const aboutMeLink = document.getElementById('link-about-me');
-  const projectsLink = document.getElementById('link-projects');
-  const contactLink = document.getElementById('link-contact');
-
-  // Contenedores de contenido
-  const centeredContent = document.getElementById('centered-content');
-  const aboutMeView = document.getElementById('about-me-view');
-  const projectsView = document.getElementById('proyects-view');
-  const contactIcons = document.getElementById('contact-icons');
-
-  // Función para mostrar un elemento y ocultar los demás
-  function showElement(elementIdToShow) {
-    // Array con todos los posibles ID de elementos a mostrar/ocultar
-    const allElements = ['centered-content', 'about-me-view', 'proyects-view', 'contact-icons'];
-
-    // Ocultar todos los elementos
-    allElements.forEach(elementId => {
-      const element = document.getElementById(elementId);
-      element.classList.add('hidden');
-    });
-
-    // Mostrar solo el elemento relevante
-    const elementToShow = document.getElementById(elementIdToShow);
-    elementToShow.classList.remove('hidden');
-  }
-
-  aboutMeLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    showElement('about-me-view');
-  });
-
-  projectsLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    showElement('proyects-view');
-  });
-
-  contactLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    showElement('contact-icons');
-  });
-});
-
-document.querySelectorAll('.card-container').forEach((card) => {
-  card.addEventListener('click', () => {
-    card.querySelector('.card-flip').classList.toggle('is-flipped');
-  });
-});
-
-// Función para mostrar u ocultar los íconos de contacto
-function toggleContactIcons(shouldShow) {
-  const contactIcons = document.getElementById('contact-icons');
-  if (shouldShow) {
-    contactIcons.classList.remove('hidden');
-  } else {
-    contactIcons.classList.add('hidden');
-  }
-}
-
-// Llama a esta función cada vez que se cambie la sección, por ejemplo:
-toggleContactIcons(true); // Para mostrar en la sección de contacto
-toggleContactIcons(false); // Para ocultar en otras secciones
-
-
-
-
-
 
 
